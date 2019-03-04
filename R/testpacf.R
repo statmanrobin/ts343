54aa093c63723cbf53a9b06baacfdbea9a8db11c
Robin Lock <rlock@stlawu.edu>
2019-03-04 11:36:51 -0500

    Fis rkk notation

--- R/testpacf.R
+++ R/testpacf.R
@@ -1,20 +1,20 @@
 #' Provide test statistics for partial autocorrelations
 #'
-#' @param seriesacf  The result of a partial autocorreltion calculation, e.g. from pacf( ) or slupacf( )
+#' @param seriespacf  The result of a partial autocorreltion calculation, e.g. from pacf( ) or slupacf( )
 #'
-#' @return A dataframe with lag, partial autocorrelation (phikk), std. error of phikk (Sphikk), test statistic (tsphikk) and Ljung-Box statistic (LBQ)
+#' @return A dataframe with lag, partial autocorrelation (rkk), std. error of rkk (Srkk), test statistic (tsrkk)
 #'
 #' @export
 #'
 
 testpacf=function(seriespacf){
-  phikk=seriespacf$acf
+  rkk=seriespacf$acf
   n=seriespacf$n.used               #find the sample size for the series
-  Sphikk=1/sqrt(n)
-  lag=1:length(phikk)
-  tsphikk=phikk/Sphikk
-  phikk=round(phikk,3)
-  Sphikk=round(Sphikk,5)
-  tsphikk=round(tsphikk,2)
-  data.frame(lag,phikk,Sphikk,tsphikk)
+  Srkk=1/sqrt(n)
+  lag=1:length(rkk)
+  tsrkk=rkk/Srkk
+  rkk=round(rkk,3)
+  Srkk=round(Srkk,5)
+  tsrkk=round(tsrkk,2)
+  data.frame(lag,rkk,Srkk,tsrkk)
 }
